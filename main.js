9570cd02e59b79375348882eaedd87564aa3f074
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-12-01 15:17:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier("MobileNet",modelloaded);
+}
+function modelloaded(){
+  console.log("Model Loaded");
 }
 
-
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video,getResults);
+}
+function getResults(error,results){
+if(error){
+  console.error(error);
+}else{
+  console.log(results);
+  document.getElementById("object_name").innerHTML=results[0].label;
+  document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+}
+}
 
